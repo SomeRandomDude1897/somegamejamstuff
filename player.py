30735bf7efd17dd969f754aed09540eddf9f2b0c
maxim <st119024@student.spbu.ru>
2023-11-25 19:59:20 +0000

    дописал прыжок, падение

--- player.py
+++ player.py
@@ -1,6 +1,3 @@
-# import pygame
-import math
-import time
 from object import Object
 
 
@@ -17,36 +14,37 @@ class Player(Object):
         self.y_delta = 0
         self.x = start_x
         self.y = start_y
+        self.g = 9.81
         self.max_jump_height = 0
 
     def GetAlive(self):
         return self.alive
 
-    def Move(self, move_right):
-        '''move_right - bool значение, отвечающее за направление движения вправо
-        от времени будет зависеть его скорость, разгон по времени планируется 1 секунда'''
-        start = time.time()
-        if start < 1:
-            self.x_velocity = self.base_x_velocity * start
+    def SetAlive(self, isAlive):
+        self.alive = isAlive
+
+    def MoveX(self, moveRight, timeMoving):
+        '''moveRight - bool значение, отвечающее за направление движения вправо
+        от времени будет зависеть его скорость, разгон по времени планируется 1 секунда
+        timeMoving - извне получать сколько времени уже двигается персонаж'''
+        if timeMoving < 1:
+            self.x_velocity = self.base_x_velocity * timeMoving
         else:
             self.x_velocity = self.base_x_velocity
-        if move_right:
+        if moveRight:
             self.x += self.x_velocity
         else:
             self.x -= self.x_velocity
 
+    def Falling(self, timeMoving):
+        '''свободное падение персонажа'''
+        self.y_velocity -= self.g * (timeMoving) ** 2 // 2
+        self.y += self.y_velocity
+
     def Jump(self):
-        '''проблема, как понять, когда нужно остановиться уменьшать координату по y, т.к.
-        неизвестно, где приземлится персонаж'''
-        prev_y = self.y
-        start = time.time()
-        g = 9.81 # условно такое будет ускорение
-        stop_jumping = True
-        while not stop_jumping: # прыжок с положительной скоростью
-            self.y_velocity = self.base_y_velocity - g*(start)**2//2
-            self.y += self.base_y_velocity
-            # действие по изменению stop_jumping
-        self.y_velocity = 0
+        '''после вызова jump нужно вызввать MoveY, пока не столкнется снизу с поверхностью'''
+        self.y_velocity = self.base_y_velocity
+        self.Falling(0)
 
     def ResetXVelocity(self):
         '''если персонаж уперся в стенку'''
